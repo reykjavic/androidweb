ef395bb4a3c2a41c9a4ed214b780268f18f6fd27
reykjavic <247reyk@gmail.com>
2017-02-02 18:28:48 +0100

    clean seed

--- app/src/main/assets/www/index.js
+++ app/src/main/assets/www/index.js
@@ -6,16 +6,3 @@ function saySomething() {
   var out = android.getText(1);
   alert(out);
 }
-
-function getWorkers() {
-  return JSON.parse(android.getWorkers());
-}
-
-function loadWorkers() {
-  var workers = getWorkers();
-
-  for (var index in workers) {
-    $('#workers').append('<li>' + workers[index] + '</li>')
-  }
-}
-
